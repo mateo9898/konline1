13b604762eae8246223d27bd77da04a7f39cba50
VEEX <czarnytroll@gmail.com>
2020-04-20 12:27:59 +0200

    ConsCreate view

--- config/routes.php
+++ config/routes.php
@@ -7,18 +7,26 @@ use Slim\App;
 use Slim\Routing\RouteCollectorProxy;
 
 return function (App $app) {
-    $app->get('/old', \App\Action\Home\HomeAction::class)->setName('root');
 
-    $app->get('/home', \App\Action\Main\MainAction::class)->setName('home');
-    
+    //====================== STRONA GŁÓWNA ========================================
     $app->get('/', \App\Action\Admin\AdminAction::class)->setName('main');
+
+    //================== DODAWANIE KONSULTACJI ====================================
+    $app->get('/add', \App\Action\Cons\ConsCreateAction::class)->setName('new-cons');
+
+
+    //======================== LOGOWANIE ==========================================
+    $app->get('/login', \App\Action\Login\LoginAction::class)->setName('login');
+    $app->post('/login', \App\Action\Login\LoginSubmitAction::class);
+
+
+
+
+
 
     $app->get('/hello/{name}', \App\Action\Hello\HelloAction::class)->setName('hello');
 
     $app->post('/api/users', \App\Action\User\UserCreateAction::class)->setName('api-user-create');
-
-    $app->get('/login', \App\Action\Login\LoginAction::class)->setName('login');
-    $app->post('/login', \App\Action\Login\LoginSubmitAction::class);
 
     $app->get('/register', \App\Action\User\UserCreateAction::class)->setName('register');
     $app->post('/register', \App\Action\User\UserSubmitAction::class);
@@ -28,9 +36,11 @@ return function (App $app) {
 
     $app->get('/mail', \App\Action\Mail\MailAction::class)->setName('mail');
 
-    
     $app->get('/datatable', \App\Action\Cons\ConsListAction::class)->setName('user-list');
     $app->post('/datatable', \App\Action\Cons\ConsListDataTableAction::class)->setName('user-datatable');
+
+    $app->get('/old', \App\Action\Home\HomeAction::class)->setName('root');
+    $app->get('/home', \App\Action\Main\MainAction::class)->setName('home');
 
 
     // $app->group('/datatable', function (RouteCollectorProxy $group) {
