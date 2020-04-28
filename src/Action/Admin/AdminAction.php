d17af0d132af7321f79bd3694cd7e274f61ff6cf
VEEX <czarnytroll@gmail.com>
2020-04-28 15:04:29 +0200

    SendMail class

--- src/Action/Admin/AdminAction.php
+++ src/Action/Admin/AdminAction.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Action\Admin;
-require __DIR__.'/../../../vendor/autoload.php';
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -10,16 +9,6 @@ use Slim\Views\Twig;
 use App\Repository\QueryFactory;
 use App\Repository\DataTableRepository;
 use App\Repository\RepositoryInterface;
-
-echo(__DIR__.'\...\..\..\vendor\PHPMailer\src\Exception.php');
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\SMTP;
-
-require __DIR__.'./../../../vendor/phpmailer/phpmailer/src/Exception.php';
-require __DIR__.'./../../../vendor/phpmailer/phpmailer/src/PHPMailer.php';
-require __DIR__.'./../../../vendor/phpmailer/phpmailer/src/SMTP.php';
 
 
 /**
@@ -32,6 +21,7 @@ final class AdminAction
      */
     private $twig;
 
+
      private $queryFactory;
     private $dataTable;
     /**
@@ -42,7 +32,6 @@ final class AdminAction
     public function __construct(Twig $twig, QueryFactory $queryFactory, DataTableRepository $dataTableRepository)
     {
         $this->twig = $twig;
-        
         $this->queryFactory = $queryFactory;
         $this->dataTable = $dataTableRepository;
     }
@@ -60,47 +49,6 @@ final class AdminAction
         // $viewData = [
         //     'now' => date('d.m.Y H:i:s'),
         // ];
-
-
-
-        $mail = new PHPMailer(true);
-        try {
-            //Server settings
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-            $mail->isSMTP();                                            // Send using SMTP
-            $mail->Host       = 'smtp.mailtrap.io';                    // Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-            $mail->Username   = '63bd291a50651f';                     // SMTP username
-            $mail->Password   = 'aea8eff261d903';                               // SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-            $mail->Port       = 25;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-        
-            //Recipients
-            $mail->setFrom('poczta@mailtrap.io', 'Administrator');
-            $mail->addAddress('mateusz.wrzol@o2.pl', 'Joe User');     // Add a recipient
-            // $mail->addAddress('ellen@example.com');               // Name is optional
-            $mail->addReplyTo('administracjaserwisu@example.com', 'Information');
-            // $mail->addCC('cc@example.com');
-            // $mail->addBCC('bcc@example.com');
-        
-            // Attachments
-            // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-        
-            // Content
-            $mail->isHTML(true);                                  // Set email format to HTML
-            $mail->Subject = 'Here is the subject';
-            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-        
-            $html = new \Html2Text\Html2Text($mail->Body);
-            $mail->AltBody = $html->getText();
-        
-            $mail->send();
-            echo 'Message has been sent';
-        } catch (Exception $e) {
-            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-        }
-
 
         if(isset($_GET['id_cons'])){
             $this->queryFactory->newUpdate('consultation',['accept' => 1])->andWhere(['id_consultation' => $_GET['id_cons']])->execute();
