490385b20a99c641a5238e8833c7f2a6d7fe8c8c
mateo9898 <mateusz.wrzol@o2.pl>
2020-04-30 13:06:00 +0200

    cons add

--- src/Domain/Day/Repository/DayGeneratorRepository.php
+++ src/Domain/Day/Repository/DayGeneratorRepository.php
@@ -36,7 +36,37 @@ class DayGeneratorRepository implements RepositoryInterface
      */
     public function insertDay(DayCreatorData $day): int
     {
+        $d;
+
+        if($day->id_day1==1){
+            $d = date("2020-12-7");
+        }else if($day->id_day1==2){
+            $d = date("2020-12-8");
+        }else if($day->id_day1==3){
+            $d = date("2020-12-9");
+        }else if($day->id_day1==4){
+            $d = date("2020-12-10");
+        }else{
+            $d = date("2020-12-11");
+        }
+        
+
         $this->queryFactory->newDelete('consultation')->execute();
+        $values = [
+            'id_consultation' => '222',
+            'start_date' => $d,
+            'start_hour' => $day->start_cons,
+            'subject'=>'PAM',
+            'end_hour' => $day->end_cons,
+            'name'=>'cos',
+            'surname'=>'cos',
+            'email'=>'a@a.pl',
+            'accept'=>1,
+            'id_user_FK'=>1,
+            'id_day_FK'=>$day->id_day1,
+        ];
+        
+        
         $this->queryFactory->newDelete('day')->execute();
         $row = [
             'id_day' =>$day->id_day1,
@@ -46,7 +76,9 @@ class DayGeneratorRepository implements RepositoryInterface
             'id_owner2_FK' => $day->id_owner2_FK,
         ];
 
-        return (int)$this->queryFactory->newInsert(TableName::DAY, $row)->execute()->lastInsertId();
+        $this->queryFactory->newInsert(TableName::DAY, $row)->execute()->lastInsertId();
+        
+        return (int)$this->queryFactory->newInsert('consultation', $values)->execute()->lastInsertId();
     }
     public function insertDay2(DayCreatorData $day): int
     {
@@ -58,7 +90,37 @@ class DayGeneratorRepository implements RepositoryInterface
             'id_owner2_FK' => $day->id_owner2_FK,
         ];
         if($day->day_name2!='--brak--'){
-            return (int)$this->queryFactory->newInsert(TableName::DAY, $row)->execute()->lastInsertId();
+            $d1;
+
+        if($day->id_day2==1){
+            $d1 = date("2020-12-7");
+        }else if($day->id_day2==2){
+            $d1 = date("2020-12-8");
+        }else if($day->id_day2==3){
+            $d1 = date("2020-12-9");
+        }else if($day->id_day2==4){
+            $d1 = date("2020-12-10");
+        }else{
+            $d1 = date("2020-12-11");
+        }
+        $values2 = [
+            'id_consultation' => '223',
+            'start_date' => $d1,
+            'start_hour' => $day->start_cons,
+            'subject'=>'PAM',
+            'end_hour' => $day->end_cons,
+            'name'=>'cos',
+            'surname'=>'cos',
+            'email'=>'a@a.pl',
+            'accept'=>1,
+            'id_user_FK'=>1,
+            'id_day_FK'=>$day->id_day2,
+        ];
+        
+    }
+        if($day->day_name2!='--brak--'){
+            $this->queryFactory->newInsert(TableName::DAY, $row)->execute();
+            return (int)$this->queryFactory->newInsert('consultation', $values2)->execute()->lastInsertId();
         }
         else{
             return 0;
